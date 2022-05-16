9402ec01cd1fd5f7b0616ec5da063b2dc1aee5d5
Slyline-oss <60854038+Slyline-oss@users.noreply.github.com>
2022-05-16 20:02:40 +0300

    Add files via upload

---
+++ lzw_code/src/src/WriteFile.java
@@ -0,0 +1,38 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class WriteFile {
+
+    private String resultFile;
+    private ArrayList<String> stringToWrite;
+
+    public WriteFile(String resultFile, ArrayList<String> stringToWrite) {
+        this.resultFile = resultFile;
+        this.stringToWrite = stringToWrite;
+    }
+
+    public void writeIntoFile() throws IOException {
+        File file = new File(resultFile);
+        if (file.createNewFile()) System.out.println("file created: " + file.getCanonicalPath());
+        else System.out.println("file already exists at location: " + file.getCanonicalPath());
+
+        BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+        String s;
+        for (int i = 0; i < stringToWrite.size(); i++) {
+            s = stringToWrite.get(i);
+            if (Objects.equals(s, "")){
+                if (i != stringToWrite.size()-1) bw.newLine();
+            } else {
+                bw.write(s + " ");
+            }
+
+        }
+        bw.close();
+     }
+
+
+}
